632ff912dcc168719dd5a9d1ef9354fb1d90317f
rachid <rachid98baghrosse@gmail.com>
2024-07-24 15:28:38 +0200

    fix: rebuild epics-servide, resolve vulnerable dependencies and refactor some code

--- epcis_nimble_server/src/test/java/eu/nimble/service/epcis/db/MongoDBConfigurationTest.java
+++ epcis_nimble_server/src/test/java/eu/nimble/service/epcis/db/MongoDBConfigurationTest.java
@@ -1,23 +1,10 @@
 package eu.nimble.service.epcis.db;
 
-import java.util.List;
-import java.util.Set;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.ConfigFileApplicationContextInitializer;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import com.mchange.v2.beans.swing.TestBean;
 
 //@ContextConfiguration(classes = MongoConfig.class)
 @PropertySource(value = "classpath:application.properties")
@@ -29,18 +16,17 @@ public class MongoDBConfigurationTest {
     @Value("${server.servlet-path}")
     public String mongoHost;
 
-    
+
 //	 @Autowired
 //	    private MongoConfig config;
 
-	   @Test
-	    public void test() {
-		   System.out.println("collectionName==="+mongoHost);  
+    @Test
+    public void test() {
+        System.out.println("collectionName===" + mongoHost);
 //	    	System.out.println("collectionName==="+config.mongoDB);  
 //		     List<String> collectionNames = config.mongoClient().getDatabaseNames();
 //		      for (String name : collectionNames) {  
 //		            System.out.println("collectionName==="+name);  
 //		      }  
-	    }
-	    
+    }
 }
